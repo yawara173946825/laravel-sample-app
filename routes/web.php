3a6014c2b0b961de3197e7600e03542e4df67d1e
yawara173946825 <yawara173946825@gmail.com>
2024-11-13 08:28:32 +0900

    feat: // homeページの作成
    homeページのルーティングとbladeの作成。こんにちはだけ表示される程度。

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// home画面のルーティング定義
+Route::get('home', function(){
+    return view('home');
+});

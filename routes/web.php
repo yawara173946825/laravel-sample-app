07ab7f7b747014c860729daee86f5c829bf5b4f0
yawara173946825 <yawara173946825@gmail.com>
2024-11-16 17:26:28 +0900

    feat: // ユーザー登録機能の実装
    ユーザーが新規登録をする機能を実装した

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,12 @@ Route::get('/', function () {
 Route::get('home', function(){
     return view('home');
 });
+
+// 新規登録ページ
+Route::get('/register', [App\Http\Controllers\RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('register');
+
+// 新規登録処理
+Route::post('/register', [App\Http\Controllers\RegisterController::class, 'store'])
+    ->middleware('guest');
